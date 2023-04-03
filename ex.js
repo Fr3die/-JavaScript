8142084342cd01e51729e804dcbd80590eed80f9
Lucas Vinicius <lvininicius@gmail.com>
2023-04-03 10:44:43 -0300

    utilizando o swicht

---
+++ ex.js
@@ -0,0 +1,15 @@
+let teste =parseInt(prompt("Digite uma opção"));
+switch(teste){
+    case 1:
+        document.write("Primeira opção")
+        break;
+    case 2:
+        document.write("Segunda opção")
+        break;
+    case 3:
+        document.write("terceira opção")
+        break;
+    default:
+        document.write("Caso não seja a primeira e nem a segunda opção")
+        break;
+}
